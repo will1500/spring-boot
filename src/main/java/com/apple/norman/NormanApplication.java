99574640011d147204bb9010ce18715c782310c8
will <will@wills-MacBook-Pro.local>
2024-10-03 15:26:19 -0700

    Started over with Spring security and Spring web dependencies from Spring Init website

---
+++ src/main/java/com/apple/norman/NormanApplication.java
@@ -0,0 +1,13 @@
+package com.apple.norman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NormanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apple/norman/NormanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.norman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
